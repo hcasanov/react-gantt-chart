23d7a95093b0e0315624d5151856ace9be0a2b56
noelledeur <noelledeur@gmail.com>
2024-05-09 10:58:05 +0200

    Remove header

--- src/lib/containers/TaskList/components/TaskListHeader.tsx
+++ src/lib/containers/TaskList/components/TaskListHeader.tsx
@@ -6,77 +6,77 @@ import { OptionalKeys } from "../../../types/custom";
 
 // *** TYPES ***
 export interface IProps {
-	columns?: (JSX.Element | string)[];
-	// style
-	rootStyle?: React.CSSProperties;
-	headerStyle?: React.CSSProperties;
-	columnStyle?: React.CSSProperties;
-	columnSeparatorStyle?: React.CSSProperties;
+  columns?: (JSX.Element | string)[];
+  // style
+  rootStyle?: React.CSSProperties;
+  headerStyle?: React.CSSProperties;
+  columnStyle?: React.CSSProperties;
+  columnSeparatorStyle?: React.CSSProperties;
 }
 type TOptionalPropsKeys = Exclude<OptionalKeys<IProps>, undefined>;
 type TOptionalProps = Required<Pick<IProps, TOptionalPropsKeys>>;
 
 export const defaultProps: TOptionalProps = {
-	columns: ["Name", "From", "To"],
-	// style
-	rootStyle: {
-		display: "table",
-		borderBottom: "#e6e4e4 1px solid",
-		borderTop: "#e6e4e4 1px solid",
-		borderLeft: "#e6e4e4 1px solid",
-	},
-	headerStyle: {
-		display: "table-row",
-		listStyle: "none",
-	},
-	columnStyle: {
-		display: "table-cell",
-		verticalAlign: "middle",
-	},
-	columnSeparatorStyle: {
-		borderRight: "1px solid rgb(196, 196, 196)",
-		opacity: 1,
-		marginLeft: "-2px",
-	},
+  columns: ["Name"],
+  // style
+  rootStyle: {
+    display: "table",
+    borderBottom: "#e6e4e4 1px solid",
+    borderTop: "#e6e4e4 1px solid",
+    borderLeft: "#e6e4e4 1px solid",
+  },
+  headerStyle: {
+    display: "table-row",
+    listStyle: "none",
+  },
+  columnStyle: {
+    display: "table-cell",
+    verticalAlign: "middle",
+  },
+  columnSeparatorStyle: {
+    borderRight: "1px solid rgb(196, 196, 196)",
+    opacity: 1,
+    marginLeft: "-2px",
+  },
 };
 
 const TaskListHeader = (props: IProps & typeof defaultProps) => {
-	// *** PROPS ***
-	const {
-		columns,
-		// style
-		rootStyle,
-		headerStyle,
-		columnStyle,
-		columnSeparatorStyle,
-	} = props;
+  // *** PROPS ***
+  const {
+    columns,
+    // style
+    rootStyle,
+    headerStyle,
+    columnStyle,
+    columnSeparatorStyle,
+  } = props;
 
-	// *** CONDITIONALS ***
-	const headerBody: JSX.Element[] = [];
-	columns.forEach((column, index) => {
-		const columnElement = (
-			<React.Fragment key={index}>
-				{/* COLUMN */}
-				<div style={columnStyle}>{column}</div>
+  // *** CONDITIONALS ***
+  const headerBody: JSX.Element[] = [];
+  columns.forEach((column, index) => {
+    const columnElement = (
+      <React.Fragment key={index}>
+        {/* COLUMN */}
+        <div style={columnStyle}>{column}</div>
 
-				{/* SEPARATOR */}
-				{index !== columns.length - 1 && <div style={columnSeparatorStyle} />}
-			</React.Fragment>
-		);
+        {/* SEPARATOR */}
+        {index !== columns.length - 1 && <div style={columnSeparatorStyle} />}
+      </React.Fragment>
+    );
 
-		headerBody.push(columnElement);
-	});
+    headerBody.push(columnElement);
+  });
 
-	return (
-		// ROOT
-		<div style={rootStyle}>
-			{/* HEADER */}
-			<div style={headerStyle}>
-				{/* BODY */}
-				{headerBody}
-			</div>
-		</div>
-	);
+  return (
+    // ROOT
+    <div style={rootStyle}>
+      {/* HEADER */}
+      <div style={headerStyle}>
+        {/* BODY */}
+        {headerBody}
+      </div>
+    </div>
+  );
 };
 TaskListHeader.defaultProps = defaultProps;
 
--- src/lib/containers/TaskList/components/TaskListTable.tsx
+++ src/lib/containers/TaskList/components/TaskListTable.tsx
@@ -9,191 +9,191 @@ import { OptionalKeys } from "../../../types/custom";
 const localeDateStringCache: { [key: string]: string } = {};
 
 const toLocaleDateStringFactory =
-	(locale: string) =>
-	(date: Date, dateTimeOptions: Intl.DateTimeFormatOptions) => {
-		const key = date.toString();
-		let lds = localeDateStringCache[key];
-		if (!lds) {
-			lds = date.toLocaleDateString(locale, dateTimeOptions);
-			localeDateStringCache[key] = lds;
-		}
-		return lds;
-	};
+  (locale: string) =>
+  (date: Date, dateTimeOptions: Intl.DateTimeFormatOptions) => {
+    const key = date.toString();
+    let lds = localeDateStringCache[key];
+    if (!lds) {
+      lds = date.toLocaleDateString(locale, dateTimeOptions);
+      localeDateStringCache[key] = lds;
+    }
+    return lds;
+  };
 
 // *** TYPES ***
 const dateTimeOptions: Intl.DateTimeFormatOptions = {
-	weekday: "short",
-	year: "numeric",
-	month: "long",
-	day: "numeric",
+  weekday: "short",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
 };
 
 export interface IProps {
-	tasks: Task[];
-	locale: string;
-	expanderSymbolOpen?: JSX.Element | string;
-	expanderSymbolClose?: JSX.Element | string;
-	expanderSymbolEmpty?: JSX.Element | string;
-	onExpanderClick: (task: Task) => void;
-	// conditionals
-	// TODO: MAKE MORE FLEXIBLE RENDERING (TILL NOW LEAVE IT LIKE SO)
-	showStartDateColumn?: boolean;
-	showEndDateColumn?: boolean;
-	// styles
-	rootStyle?: React.CSSProperties;
-	tableRowStyle?: React.CSSProperties;
-	tableRowStyleProject?: React.CSSProperties;
-	tableCellStyle?: React.CSSProperties;
-	tableCellStyleProject?: React.CSSProperties;
-	tableCellStyleTask?: React.CSSProperties;
-	tableCellStyleMileStone?: React.CSSProperties;
-	tableCellWrapperStyle?: React.CSSProperties;
-	expanderStyle?: React.CSSProperties;
-	expanderEmptyStyle?: React.CSSProperties;
+  tasks: Task[];
+  locale: string;
+  expanderSymbolOpen?: JSX.Element | string;
+  expanderSymbolClose?: JSX.Element | string;
+  expanderSymbolEmpty?: JSX.Element | string;
+  onExpanderClick: (task: Task) => void;
+  // conditionals
+  // TODO: MAKE MORE FLEXIBLE RENDERING (TILL NOW LEAVE IT LIKE SO)
+  showStartDateColumn?: boolean;
+  showEndDateColumn?: boolean;
+  // styles
+  rootStyle?: React.CSSProperties;
+  tableRowStyle?: React.CSSProperties;
+  tableRowStyleProject?: React.CSSProperties;
+  tableCellStyle?: React.CSSProperties;
+  tableCellStyleProject?: React.CSSProperties;
+  tableCellStyleTask?: React.CSSProperties;
+  tableCellStyleMileStone?: React.CSSProperties;
+  tableCellWrapperStyle?: React.CSSProperties;
+  expanderStyle?: React.CSSProperties;
+  expanderEmptyStyle?: React.CSSProperties;
 }
 type TOptionalPropsKeys = Exclude<OptionalKeys<IProps>, undefined>;
 type TOptionalProps = Required<Pick<IProps, TOptionalPropsKeys>>;
 
 export const defaultProps: TOptionalProps = {
-	expanderSymbolOpen: "▼",
-	expanderSymbolClose: "▶",
-	expanderSymbolEmpty: "",
-	// conditionals
-	showStartDateColumn: true,
-	showEndDateColumn: true,
-	// styles
-	rootStyle: {
-		display: "table",
-		borderBottom: "#e6e4e4 1px solid",
-		borderLeft: "#e6e4e4 1px solid",
-	},
-	tableRowStyle: {
-		display: "table-row",
-		textOverflow: "ellipsis",
-	},
-	tableRowStyleProject: {},
-	tableCellStyle: {
-		display: "table-cell",
-		verticalAlign: "middle",
-		whiteSpace: "nowrap",
-		overflow: "hidden",
-		textOverflow: "ellipsis",
-	},
-	tableCellStyleProject: {},
-	tableCellStyleTask: {},
-	tableCellStyleMileStone: {},
-	tableCellWrapperStyle: {
-		display: "flex",
-	},
-	expanderStyle: {
-		color: "rgb(86 86 86)",
-		fontSize: "0.6rem",
-		padding: "0.15rem 0.2rem 0rem 0.2rem",
-		userSelect: "none",
-		cursor: "pointer",
-	},
-	expanderEmptyStyle: {
-		fontSize: "0.6rem",
-		paddingLeft: "1rem",
-		userSelect: "none",
-	},
+  expanderSymbolOpen: "▼",
+  expanderSymbolClose: "▶",
+  expanderSymbolEmpty: "",
+  // conditionals
+  showStartDateColumn: false,
+  showEndDateColumn: false,
+  // styles
+  rootStyle: {
+    display: "table",
+    borderBottom: "#e6e4e4 1px solid",
+    borderLeft: "#e6e4e4 1px solid",
+  },
+  tableRowStyle: {
+    display: "table-row",
+    textOverflow: "ellipsis",
+  },
+  tableRowStyleProject: {},
+  tableCellStyle: {
+    display: "table-cell",
+    verticalAlign: "middle",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  },
+  tableCellStyleProject: {},
+  tableCellStyleTask: {},
+  tableCellStyleMileStone: {},
+  tableCellWrapperStyle: {
+    display: "flex",
+  },
+  expanderStyle: {
+    color: "rgb(86 86 86)",
+    fontSize: "0.6rem",
+    padding: "0.15rem 0.2rem 0rem 0.2rem",
+    userSelect: "none",
+    cursor: "pointer",
+  },
+  expanderEmptyStyle: {
+    fontSize: "0.6rem",
+    paddingLeft: "1rem",
+    userSelect: "none",
+  },
 };
 
 const TaskListTable = (props: IProps & typeof defaultProps) => {
-	// *** PROPS ***
-	const {
-		tasks,
-		locale,
-		expanderSymbolOpen,
-		expanderSymbolClose,
-		expanderSymbolEmpty,
-		onExpanderClick,
-		// conditionals
-		showStartDateColumn,
-		showEndDateColumn,
-		// styles
-		rootStyle,
-		tableRowStyle,
-		tableRowStyleProject,
-		tableCellStyle,
-		tableCellStyleProject,
-		tableCellStyleTask,
-		tableCellStyleMileStone,
-		tableCellWrapperStyle,
-		expanderStyle,
-		expanderEmptyStyle,
-	} = props;
+  // *** PROPS ***
+  const {
+    tasks,
+    locale,
+    expanderSymbolOpen,
+    expanderSymbolClose,
+    expanderSymbolEmpty,
+    onExpanderClick,
+    // conditionals
+    showStartDateColumn,
+    showEndDateColumn,
+    // styles
+    rootStyle,
+    tableRowStyle,
+    tableRowStyleProject,
+    tableCellStyle,
+    tableCellStyleProject,
+    tableCellStyleTask,
+    tableCellStyleMileStone,
+    tableCellWrapperStyle,
+    expanderStyle,
+    expanderEmptyStyle,
+  } = props;
 
-	// *** HANDLERS ***
-	const toLocaleDateString = useMemo(
-		() => toLocaleDateStringFactory(locale),
-		[locale]
-	);
+  // *** HANDLERS ***
+  const toLocaleDateString = useMemo(
+    () => toLocaleDateStringFactory(locale),
+    [locale]
+  );
 
-	return (
-		// ROOT
-		<div style={rootStyle}>
-			{/* TABLE ROWS */}
-			{tasks.map((t) => {
-				// expander
-				let expanderSymbol: JSX.Element | string = expanderSymbolEmpty;
-				if (t.hideChildren === true) {
-					expanderSymbol = expanderSymbolClose;
-				} else if (t.hideChildren === false) {
-					expanderSymbol = expanderSymbolOpen;
-				}
+  return (
+    // ROOT
+    <div style={rootStyle}>
+      {/* TABLE ROWS */}
+      {tasks.map((t) => {
+        // expander
+        let expanderSymbol: JSX.Element | string = expanderSymbolEmpty;
+        if (t.hideChildren === true) {
+          expanderSymbol = expanderSymbolClose;
+        } else if (t.hideChildren === false) {
+          expanderSymbol = expanderSymbolOpen;
+        }
 
-				// styles
-				const rowStyle: React.CSSProperties = {
-					...tableRowStyle,
-					...(t.type === "project" ? tableRowStyleProject : {}),
-				};
+        // styles
+        const rowStyle: React.CSSProperties = {
+          ...tableRowStyle,
+          ...(t.type === "project" ? tableRowStyleProject : {}),
+        };
 
-				const cellStyle: React.CSSProperties = {
-					...tableCellStyle,
-					...(t.type === "project" ? tableCellStyleProject : {}),
-					...(t.type === "task" ? tableCellStyleTask : {}),
-					...(t.type === "milestone" ? tableCellStyleMileStone : {}),
-				};
+        const cellStyle: React.CSSProperties = {
+          ...tableCellStyle,
+          ...(t.type === "project" ? tableCellStyleProject : {}),
+          ...(t.type === "task" ? tableCellStyleTask : {}),
+          ...(t.type === "milestone" ? tableCellStyleMileStone : {}),
+        };
 
-				return (
-					// TABLE ROW
-					<div style={rowStyle} key={`${t.id}row`}>
-						{/* PRIMARY CELL */}
-						<div style={cellStyle} title={t.name}>
-							{/* TABLE CELL WRAPPER */}
-							<div style={tableCellWrapperStyle}>
-								{/* EXPANDER */}
-								<div
-									style={expanderSymbol ? expanderStyle : expanderEmptyStyle}
-									onClick={() => onExpanderClick(t)}
-								>
-									{expanderSymbol}
-								</div>
+        return (
+          // TABLE ROW
+          <div style={rowStyle} key={`${t.id}row`}>
+            {/* PRIMARY CELL */}
+            <div style={cellStyle} title={t.name}>
+              {/* TABLE CELL WRAPPER */}
+              <div style={tableCellWrapperStyle}>
+                {/* EXPANDER */}
+                <div
+                  style={expanderSymbol ? expanderStyle : expanderEmptyStyle}
+                  onClick={() => onExpanderClick(t)}
+                >
+                  {expanderSymbol}
+                </div>
 
-								{/* TEXT */}
-								<div>{t.name}</div>
-							</div>
-						</div>
+                {/* TEXT */}
+                <div>{t.name}</div>
+              </div>
+            </div>
 
-						{/* START DATE CELL */}
-						{showStartDateColumn === true && (
-							<div style={cellStyle}>
-								&nbsp;{toLocaleDateString(t.start, dateTimeOptions)}
-							</div>
-						)}
+            {/* START DATE CELL */}
+            {showStartDateColumn === true && (
+              <div style={cellStyle}>
+                &nbsp;{toLocaleDateString(t.start, dateTimeOptions)}
+              </div>
+            )}
 
-						{/* END DATE CELL */}
-						{showEndDateColumn === true && (
-							<div style={cellStyle}>
-								&nbsp;{toLocaleDateString(t.end, dateTimeOptions)}
-							</div>
-						)}
-					</div>
-				);
-			})}
-		</div>
-	);
+            {/* END DATE CELL */}
+            {showEndDateColumn === true && (
+              <div style={cellStyle}>
+                &nbsp;{toLocaleDateString(t.end, dateTimeOptions)}
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 TaskListTable.defaultProps = defaultProps;
 
